34c786d7816ff1d9b1a175394951c31642132485
OzgeHosgor <110829260+OzgeHosgor@users.noreply.github.com>
2022-09-20 20:17:40 +0300

    mini projeler 1-23
    Java Camp

---
+++ javademos2022/variables/src/variables/main.java
@@ -0,0 +1,12 @@
+package variables;
+
+public class main {
+
+	public static void main(String[] args) {
+		int ogrenciSayisi = 11;
+		System.out.println("Öğrenci Sayım: " +ogrenciSayisi);
+		
+		
+	}
+
+}
